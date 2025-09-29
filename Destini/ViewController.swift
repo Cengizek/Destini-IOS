291a3dda4fedd0493b91941ce8eb90bdf928e2f5
Cengizek <ekmekcicengiz64@gmail.com>
2025-09-29 19:49:48 +0300

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Cengiz Ekmekçi on 29.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
